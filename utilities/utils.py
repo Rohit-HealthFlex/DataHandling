9379d80dd7e1fd755c8e5d4a25ac73fa7e837752
siddhant230 <rsiddhant73@gmail.com>
2024-02-18 14:42:36 +0530

    added local and global anglecalc logic

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,25 +1,56 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
+def show_lines_angles(out_dict):
+    for k in out_dict:
+        seg1 = out_dict[k]["seg1"]
+        seg2 = out_dict[k]["seg2"]
+        angle = out_dict[k]["angle"]
+
+        plt.plot([seg1[0][0], seg1[1][0]],
+                 [seg1[0][1], seg1[1][1]])
+        plt.plot([seg2[0][0], seg2[1][0]],
+                 [seg2[0][1], seg2[1][1]])
+        plt.title(f"{k} : angle: {angle}")
+        plt.show()
+
+
 def get_angle(x, y):
-    x = x/np.linalg.norm(x)
-    y = y/np.linalg.norm(y)
+    if x.any():
+        x = x/np.linalg.norm(x)
+    if y.any():
+        y = y/np.linalg.norm(y)
     dot_product = np.dot(x, y)
     angle = np.arccos(dot_product)
     return np.rad2deg(angle)
 
 
-def get_angle_bw_line_segments(line1: tuple, line2: tuple):
-    p0_p1 = line1[0] - line1[1]
+def get_angle_bw_line_segments(line1, line2):
+    p0_p1 = line1[1] - line1[0]
     p1_p2 = line2[0] - line2[1]
     angle = abs(get_angle(p0_p1, p1_p2))
-    return angle if angle <= 90 else angle - 90
+    return angle  # if angle <= 90 else angle - 90
 
 
-p0 = [0, 1, 3]
-p1 = [0, 0, 4]
-p2 = [2, 1, 1]
+def get_axis_wise_angles(line1, line2):
+    pairs = {"x-y": [0, 1], "y-z": [1, 2], "x-z": [0, 2]}
+    out = {}
+    for k, pair in pairs.items():
+        seg1 = (line1[0][pair], line1[1][pair])
+        seg2 = (line2[0][pair], line2[1][pair])
+        angle = get_angle_bw_line_segments(seg1, seg2)
+        out[k] = {"seg1": seg1, "seg2": seg2, "angle": angle}
+    return out
+
+
+p0 = [0, 0, 0]
+p1 = [1, 1, 0]
+p2 = [2, -1, 1]
 line1 = (np.array(p0), np.array(p1))
 line2 = (np.array(p1), np.array(p2))
 angle = get_angle_bw_line_segments(line1, line2)
 print(angle)
+
+out = get_axis_wise_angles(line1, line2)
+show_lines_angles(out)
